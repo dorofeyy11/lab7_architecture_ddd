d8a1dff5980a9d0e45f31ec43eb6ec92678c6f2a
dorofeyy11 <forofeyy@gmail.com>
2025-12-29 02:41:59 +0300

    Create fake_payment_gateway.py

---
+++ infrastructure/fake_payment_gateway.py
@@ -0,0 +1,10 @@
+class FakePaymentGateway:
+    def __init__(self):
+        self.charges = []
+
+    def charge(self, order_id, money):
+        self.charges.append({
+            "order_id": order_id,
+            "amount": money.amount,
+            "currency": money.currency,
+        })
